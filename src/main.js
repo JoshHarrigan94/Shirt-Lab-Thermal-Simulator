98d5d133c6122a3e60525a023344317c3052c9f5
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-06-16 14:02:04 +0100

    Delete src directory

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { ShirtScene } from './scene.js';
-import { UIController } from './ui.js';
-
-const container = document.getElementById('scene');
-const scene = new ShirtScene(container);
-new UIController(scene);
